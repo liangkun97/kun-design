1a80651b6d6841d16b6a051094a45ab544dec2a2
liangkun <liangkun.97@bytedance.com>
2022-01-20 21:23:20 +0800

    feat: icon

---
+++ src/components/icon/__test__/icon.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { Icon, IconProps } from "../index";
+import { icons } from "../../../shared/icons";
+
+function IconTest(icon: IconProps["icon"]) {
+  const wrapper = render(<Icon icon={"mobile"} data-testid="icon-path" />);
+  const path = wrapper.queryByTestId("icon-path").firstElementChild;
+  expect(path).toHaveAttribute("d", icons["mobile"]);
+  cleanup();
+}
+
+describe("test Icon component", () => {
+  it("it should render correct icon", () => {
+    Object.keys(icons).forEach((key) => {
+      IconTest(key as IconProps["icon"]);
+    });
+  });
+  it("it should render block", () => {
+    const wrapper = render(<Icon icon="mobile" data-testid="icon-path" />);
+    let path = wrapper.queryByTestId("icon-path");
+    expect(path).toHaveStyle("display:inline-block");
+  });
+  it("it should render correct color ", () => {
+    let wrapper = render(<Icon icon="mobile" data-testid="icon-path" />);
+    let path = wrapper.queryByTestId("icon-path");
+    expect(path).toHaveAttribute("color", "black");
+    cleanup();
+    wrapper = render(<Icon icon="mobile" color="blue" data-testid="icon-path" />);
+    path = wrapper.queryByTestId("icon-path");
+    expect(path).toHaveAttribute("color", "blue");
+  });
+});
