082f00059bc38be75607c3c8e4aa7a2287ebf97a
liangkun97 <6765254@qq.com>
2022-01-22 13:29:28 +0800

    feat: avatar

---
+++ src/components/avatar/__test__/avatar.test.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import { Avatar, AvatarSize } from "../index";
+
+describe("test Avatar component", () => {
+  it("it should render default avatar", () => {
+    let wrapper = render(<Avatar username="kun" data-testid="avatar-div" />);
+    expect(wrapper).toMatchSnapshot();
+    let div = wrapper.getByTestId("avatar-div");
+    expect(div).toBeInTheDocument();
+    let username = wrapper.getByText("k");
+    expect(username).toBeTruthy();
+  });
+  it("it should render correct size", () => {
+    let wrapper = render(<Avatar data-testid="avatar-div" />);
+    let div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle(`height:${AvatarSize.medium}px`);
+    expect(div).toHaveStyle(`width:${AvatarSize.medium}px`);
+    expect(div).toHaveStyle(`line-height:${AvatarSize.medium}px`);
+    let username = wrapper.getByTestId("avatar-div");
+    expect(username).toHaveStyle(`line-height:${AvatarSize.medium}px`);
+    cleanup();
+    wrapper = render(<Avatar size="large" data-testid="avatar-div" />);
+    expect(wrapper).toMatchSnapshot();
+    div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle(`height:${AvatarSize.large}px`);
+    expect(div).toHaveStyle(`width:${AvatarSize.large}px`);
+    expect(div).toHaveStyle(`line-height:${AvatarSize.large}px`);
+    username = wrapper.getByTestId("avatar-div");
+    expect(username).toHaveStyle(`line-height:${AvatarSize.large}px`);
+    cleanup();
+    wrapper = render(<Avatar size="small" data-testid="avatar-div" />);
+    div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle(`height:${AvatarSize.small}px`);
+    expect(div).toHaveStyle(`width:${AvatarSize.small}px`);
+    expect(div).toHaveStyle(`line-height:${AvatarSize.small}px`);
+    username = wrapper.getByTestId("avatar-div");
+    expect(username).toHaveStyle(`line-height:${AvatarSize.small}px`);
+    cleanup();
+    wrapper = render(<Avatar size="tiny" data-testid="avatar-div" />);
+    div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle(`height:${AvatarSize.tiny}px`);
+    expect(div).toHaveStyle(`width:${AvatarSize.tiny}px`);
+    expect(div).toHaveStyle(`line-height:${AvatarSize.tiny}px`);
+    username = wrapper.getByTestId("avatar-div");
+    expect(username).toHaveStyle(`line-height:${AvatarSize.tiny}px`);
+    cleanup();
+    wrapper = render(<Avatar size="medium" data-testid="avatar-div" />);
+    div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle(`height:${AvatarSize.medium}px`);
+    expect(div).toHaveStyle(`width:${AvatarSize.medium}px`);
+    expect(div).toHaveStyle(`line-height:${AvatarSize.medium}px`);
+    username = wrapper.getByTestId("avatar-div");
+    expect(username).toHaveStyle(`line-height:${AvatarSize.medium}px`);
+  });
+  it("should correct loading", () => {
+    let wrapper = render(<Avatar isLoading data-testid="avatar-div" />);
+    expect(wrapper).toMatchSnapshot();
+    let svg = wrapper.getByTestId("avatar-div");
+    expect(svg).toBeVisible();
+    cleanup();
+    wrapper = render(
+      <Avatar isLoading username="123" src="/" size="tiny" data-testid="avatar-div" />
+    );
+    svg = wrapper.getByTestId("avatar-div");
+    expect(svg).toBeVisible();
+  });
+  it("should correct img", () => {
+    let wrapper = render(<Avatar src="www.test.com" data-testid="avatar-div" />);
+    let img = wrapper.getByTestId("avatar-img");
+    expect(img.tagName).toEqual("IMG");
+    expect(img).toHaveStyle("width:100%");
+    expect(img).toHaveAttribute("src", "www.test.com");
+    expect(img).toHaveAttribute("alt", "loading");
+    cleanup();
+    wrapper = render(
+      <Avatar src="www.liang-kun.cn" username="liang-kun" data-testid="avatar-div" />
+    );
+    img = wrapper.getByTestId("avatar-img");
+    expect(img).toHaveAttribute("src", "www.liang-kun.cn");
+    expect(img).toHaveAttribute("alt", "liang-kun");
+  });
+  it("should render correct username", () => {
+    let wrapper = render(<Avatar username="liang-kun" data-testid="avatar-div" />);
+    expect(wrapper).toMatchSnapshot();
+    let div = wrapper.getByTestId("avatar-div");
+    expect(div).toHaveStyle("text-transform:uppercase");
+    let username = wrapper.getByText("l");
+    expect(username).toBeVisible();
+    cleanup();
+    wrapper = render(<Avatar username="中文汉字" data-testid="avatar-div" />);
+    username = wrapper.getByText("中");
+    expect(username).toBeTruthy();
+  });
+});
---
+++ src/components/avatar/avatar.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Avatar, AvatarSize } from "./index";
+import { withKnobs, text, boolean, select } from "@storybook/addon-knobs";
+
+export default {
+  title: "数据展示 /Avatar",
+  component: Avatar,
+  decorators: [withKnobs],
+};
+type AvatarSizeType = keyof typeof AvatarSize;
+
+export const knobsAvatar = () => (
+  <Avatar
+    size={select<AvatarSizeType>("size", Object.keys(AvatarSize) as AvatarSizeType[], "medium")}
+    username={text("username", "kun-design")}
+    src={text("src", "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png")}
+    isLoading={boolean("isLoading", false)}
+  />
+);
+
+export const large = () => (
+  <div>
+    <Avatar isLoading size="large" />
+    <Avatar size="large" username="kun-design" />
+    <Avatar
+      size="large"
+      username="kun-design"
+      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+    />
+  </div>
+);
+
+export const medium = () => (
+  <div>
+    <Avatar isLoading />
+    <Avatar username="中文" />
+    <Avatar
+      username="kun-design"
+      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+    />
+  </div>
+);
+
+export const small = () => (
+  <div>
+    <Avatar isLoading size="small" />
+    <Avatar size="small" username="kun-design" />
+    <Avatar
+      size="small"
+      username="kun-design"
+      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+    />
+  </div>
+);
+
+export const tiny = () => (
+  <div>
+    <Avatar isLoading size="tiny" />
+    <Avatar size="tiny" username="kun-design" />
+    <Avatar
+      size="tiny"
+      username="kun-design"
+      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+    />
+  </div>
+);
